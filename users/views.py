e1f50bad7e7e83c2e82b94743b5cb77417acd43d
Spence Peacock <spencerpeacock@gmail.com>
2017-10-26 23:03:31 -0500

    added a toggle menu

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
 
 # Create your views here.
+def dashboard(requests):
+    return render(request, 'users/dashboard.html')
