ff56d075b9d10c17b8613e79953a1be230d39b30
Spence Peacock <spencerpeacock@gmail.com>
2017-10-26 23:13:13 -0500

    added login-redirect settings and dashboard template

--- feelings/settings.py
+++ feelings/settings.py
@@ -124,4 +124,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
 
 # //authentication
 LOGIN_URL = 'users:login'
-LOGIN_REDIRECT_URL = 'users: dashboard'
+LOGIN_REDIRECT_URL = 'users:dashboard'
--- users/views.py
+++ users/views.py
@@ -2,5 +2,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def dashboard(requests):
+def dashboard(request):
     return render(request, 'users/dashboard.html')
