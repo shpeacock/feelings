d72fc0f0aaa569ff61c5ca5d8dea9ee808c0504f
Spence Peacock <spencerpeacock@gmail.com>
2017-10-30 20:18:43 -0500

    logout form and strong params

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-url(r'^dashboard/$', views.dashboard, name = 'dashboard'),
+    url(r'^dashboard/$', views.dashboard, name = 'dashboard'),
+    url(r'^logout/$', views.LogoutView.as_view(), name = 'logout'),
     url('^', include('django.contrib.auth.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
-
+from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import FormView
+from django.http import HttpResponseRedirect
+from . import forms
 # Create your views here.
+
 def dashboard(request):
     return render(request, 'users/dashboard.html')
+
+class LogoutView(LoginRequiredMixin,FormView):
+    form_class = forms.LogoutForm
+    template_name = 'users/logout.html'
+
+    def form_valid(self, form):
+        logout(self.request)
+        return HttpResponseRedirect(reverse('home'))
